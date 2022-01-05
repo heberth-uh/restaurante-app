43f90c8e2311717ecd3e891c5f13cd2d762c8348
heberth-uh <heberth.17070003@itsmotul.edu.mx>
2022-01-05 16:44:52 -0600

    Cambios en el setState para las variables de estado

--- screens/NewFood.js
+++ screens/NewFood.js
@@ -1,73 +1,87 @@
 import React, { useState } from 'react'
-import { View,
-         Text,
-         TextInput,
-         TouchableOpacity,
-         ScrollView,
-         StyleSheet,
-         Alert,
-         } from "react-native";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ScrollView,
+    StyleSheet,
+    Alert,
+    Button,
+} from "react-native";
 import db from "../database/firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const NewFood = () => {
 
-    const [state, setState] = useState({
-        nombre: '',
-        precio: '',
-        desc: ''
-    })
+    // const [state, setState] = useState({
+    //     nombre: 'nombre',
+    //     precio: 'precio',
+    //     desc: 'descripción'
+    // });
+    const [nombre, setNombre] = useState('');
+    const [precio, setPrecio] = useState('');
+    const [desc, setDesc] = useState('');
+
 
     const handleTextInput = (input, value) => {
-        setState({ ...setState, [input]: value})
+        // setState({ ...setState, [input]: value })
+        if (input === 'nombre') setNombre(value);
+        if (input === 'precio') setPrecio(value);
+        if (input === 'desc') setDesc(value);
     }
 
     // Creación comida a la base de datos
     const saveNewFood = async () => {
-        if (state.nombre === '') {
-            Alert.alert('Debe ingresar un nombre')
-        } 
+        if (!nombre || !precio || !desc ) {
+            Alert.alert('Por favor, llene todos los campos.')
+        }
         else {
-            
+
+            // Alert.alert(`${nombre}, ${precio}, ${desc}`);
+            console.log(`${nombre}, ${precio}, ${desc}`);
             try {
                 const docRef = await addDoc(collection(db, "users"), {
-                  first: "Ada",
-                  last: "Lovelace",
-                  born: 1815
+                    name: nombre,
+                    price: precio,
+                    desc: desc
                 });
-                console.log("Document written with ID: ", docRef.id);
-              } catch (e) {
-                console.error("Error adding document: ", e);
-              }
-            Alert.alert('¡Comida guardada!');
+                Alert.alert("¡Comida guardada! \nDocument written with ID: ", docRef.id);
+            } catch (e) {
+                console.error("Ocurrió un error al añadir un registro a la base de datos: ", e);
+            }
         }
     }
 
     return (
-        <ScrollView style={ styles.container }>
-            <Text style={ [styles.text_bold, styles.text_center, styles.title ] }>Agregar nueva comida</Text>
-            <View style={ styles.inputgroup }>
+        <ScrollView style={styles.container}>
+            <Text style={[styles.text_bold, styles.text_center, styles.title]}>Agregar nueva comida</Text>
+            <View style={styles.inputgroup}>
                 <TextInput
-                    style={ styles.inputform }
+                    style={styles.inputform}
                     placeholder='Nombre'
-                    onChange={ (value) => handleTextInput('nombre', value) } />
+                    value={nombre}
+                    onChangeText={(value) => handleTextInput('nombre', value)} />
             </View>
-            <View style={ styles.inputgroup }>
+            <View style={styles.inputgroup}>
                 <TextInput
-                    style={ styles.inputform }
+                    style={styles.inputform}
                     placeholder='Precio'
-                    onChange={ (value) => handleTextInput('precio', value) } />
+                    value={precio}
+                    onChangeText={(value) => handleTextInput('precio', value)} />
             </View>
-            <View style={ styles.inputgroup }>
+            <View style={styles.inputgroup}>
                 <TextInput
-                    style={ styles.inputform }
+                    style={styles.inputform}
                     placeholder='Descripción'
-                    onChange={ (value) => handleTextInput('desc', value) } />
+                    value={desc}
+                    onChangeText={(value) => handleTextInput('desc', value)} />
             </View>
-            <View style={ styles.inputgroup }>
+            <View style={styles.inputgroup}>
                 <TouchableOpacity
-                    style={ styles.btnguardar}
-                    onPress={ () => saveNewFood() }>
-                    <Text style={ [styles.text_white, styles.text_bold, styles.text_center] }>
+                    style={styles.btnguardar}
+                    onPress={() => saveNewFood()}>
+                    <Text style={[styles.text_white, styles.text_bold, styles.text_center]}>
                         Guardar
                     </Text>
                 </TouchableOpacity>
